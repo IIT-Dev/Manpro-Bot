d0fa83d020919884a83e8c474dc2b1f575927c5e
Alifia Rahmah <alifiarahmah03@gmail.com>
2023-06-12 23:15:41 +0700

    fix: change sheet to absolute URL

--- src/utils/fetchSheets.ts
+++ src/utils/fetchSheets.ts
@@ -1,11 +1,12 @@
 import fetch from 'node-fetch';
-import config from '../config';
 import { IProjectRequest } from '../interfaces/IProjectRequest';
 
 // TODO: Add types
 
 export async function fetchSheets() : Promise<Object> {
-    return fetch(config.sheets).then((res) => {
+    return fetch(
+        'https://script.google.com/macros/s/AKfycby5w9hVeQ_2mxVn4PywmmoP1GIR1KgbQ7YqcxF49cCO4Kg6QCZC9KUuP86hYsnrjsKH/exec',
+    ).then((res) => {
         if (!res.ok) {
             throw new Error(res.statusText);
         }
